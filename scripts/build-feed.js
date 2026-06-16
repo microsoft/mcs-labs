a6f3e80f6caa05b46432562f828abe6d537340a5
Dewain Robinson (from Dev Box) <dewainr@microsoft.com>
2026-06-16 18:34:03 -0500

    chore(feed): scaffold build-feed generator + deps

---
+++ scripts/build-feed.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const crypto = require('node:crypto');
+
+const ALL_COLLECTIONS = ['events', 'workshops', 'modules', 'labs'];
+
+module.exports = {};
+
+// Pure functions are appended below by subsequent tasks.
+// The CLI entry (if require.main === module) is added last.
