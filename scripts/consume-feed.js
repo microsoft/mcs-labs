e1b3cb0a2e0d7af33209b13133faf069f14d6ac6
Dewain Robinson (from Dev Box) <dewainr@microsoft.com>
2026-06-17 10:57:18 -0500

    chore(consume): scaffold feed consumer + ignore .feed-build

---
+++ scripts/consume-feed.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const yaml = require('js-yaml');
+
+module.exports = {};
+
+// Pure functions are appended below by subsequent tasks.
+// The CLI entry (if require.main === module) is added last.
